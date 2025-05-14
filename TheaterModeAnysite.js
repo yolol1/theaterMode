d64bb5a3912ade14f59746c10806b96bd1aedacd
Ziyueqi-V <1208896581@qq.com>
2025-05-14 10:36:47 +0800

    update: add iframe support for theater mode

--- TheaterModeAnysite.js
+++ TheaterModeAnysite.js
@@ -9,22 +9,16 @@
 // @description:zh-TW   一些網站的視頻劇院模式
 // @tag                 utilities
 // @author              Yolo
-// @match               https://mjv004.com/*
-// @match               https://missav.ai/*
-// @match               https://jable.tv/*
+// @match               *://**/*
 // @icon                data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant               GM_addStyle
 // @grant               GM_getValue
 // @grant               GM_setValue
+// @run-at              document-end
 // ==/UserScript==
 
 (function () {
     'use strict';
-    // 在这里写入你的代码...
-    const playerDict = loadPlayerDict();
-    console.log(playerDict);
-
-    const isIframe = window.top !== window.self;
     const fullscreenStyle = `
         .reset-style {
             margin: 0 !important;
@@ -40,80 +34,96 @@
             left: 0 !important;
             right: 0 !important;
             bottom: 0 !important;
+            background: rgba(0, 0, 0, 1) !important;
         }
 
         .no-scroll {
             overflow: hidden !important;
         }
     `
-
     GM_addStyle(fullscreenStyle)
-    // document.addEventListener("keydown", (e) => {
-    //     if (e.key === "Escape") {
-    //         if (!isIframe) {
-    //             const targetElement = document.querySelector("iframe");
-    //             toggleFullScreen(targetElement);
 
-    //             const innerPlayer = targetElement.contentDocument.querySelector("#player_top .plyr");
-    //             toggleFullScreen(innerPlayer);
-    //             toggleDisableScroll(document);
-    //         }
-    //         else {
-    //             const targetElement = document.querySelector("#player_top .plyr");
-    //             toggleFullScreen(targetElement);
+    let isIframeMode = window.top !== window.self;
+    function runInIframe() {
+        console.log("running in iframe");
 
-    //             const outerElement = window.parent.document.querySelector("iframe");
-    //             toggleFullScreen(outerElement);
-    //             toggleDisableScroll(window.parent.document);
-    //         }
-    //     }
-    // });
-
-    // missav
-    const playerSelector = playerDict[window.location.hostname];
-
-    const playerElement = document.querySelector(playerSelector);
-
-    document.addEventListener("keydown", (e) => {
-        if (e.key === "Escape") {
-            if (!isIframe) {
-                toggleTheaterMode(playerElement);
+        // tell top to listen message
+        window.parent.postMessage("IframeMode", "*");
+        // listen to message from top
+        window.addEventListener("message", (event) => {
+            switch (event.data) {
+                case "toggleTheaterMode":
+                    const playerDict = loadPlayerDict();
+                    const playerElement = document.querySelector(playerDict[window.location.hostname]);
+                    toggleTheaterMode(playerElement);
+                    break;
+                default:
+                    break;
             }
-            else {
-                toggleFullScreen(playerElement);
-                toggleDisableScroll(window.parent.document);
-            }
-        }
-    });
+        });
 
-    function savePlayerDict(playerDict) {
-        GM_setValue("playerDict", JSON.stringify(playerDict));
+        document.addEventListener("keydown", (e) => {
+            console.log("recieved keydown event in iframe");
+            if (e.key === "Escape") {
+                window.parent.postMessage("toggleTheaterMode", "*");
+            }
+        })
+    }
+
+    function runInTopLevel() {
+        const playerDict = loadPlayerDict();
+        let theaterSelector;
+        window.addEventListener("message", (event) => {
+            switch (event.data) {
+                case "IframeMode":
+                    isIframeMode = event.data.isIframeMode;
+                    theaterSelector = "iframe";
+                    break;
+                case "toggleTheaterMode":
+                    toggleTheaterMode(document.querySelector(theaterSelector));
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        document.addEventListener("keydown", (e) => {
+            const iframeElement = document.querySelector("iframe");
+            const playerElement = document.querySelector(playerDict[window.location.hostname]);
+            const theaterElement = playerElement || iframeElement;
+            if (e.key === "Escape") {
+                toggleTheaterMode(theaterElement);
+                iframeElement && iframeElement.contentWindow.postMessage("toggleTheaterMode", "*");
+            }
+        });
+    }
+
+    if (isIframeMode) {
+        runInIframe();
+    }
+    else {
+        runInTopLevel();
     }
 
     function loadPlayerDict() {
         const defaultPlayerDict = {
             "mjv004.com": ".plyr",
             "missav.ai": ".plyr",
-            "jable.tv": ".plyr"
+            "jable.tv": ".plyr",
+            "www.javrate.com": ".plyr"
         };
         const playerDict = GM_getValue("playerDict",);
         return playerDict || defaultPlayerDict;
     }
 
-    function toggleTheaterMode(targetElement) {
-        targetElement.classList.toggle("reset-style");
-        targetElement.classList.toggle("keep-front");
-    }
-
     function toggleClassRecursively(element, className) {
         element.classList.toggle(className);
-
         if (element.parentElement && element.parentElement.tagName !== "BODY") {
             toggleClassRecursively(element.parentElement, className);
         }
     }
 
-    function toggleFullScreen(element) {
+    function toggleTheaterMode(element) {
         toggleClassRecursively(element, "reset-style");
         toggleClassRecursively(element, "keep-front");
     }
